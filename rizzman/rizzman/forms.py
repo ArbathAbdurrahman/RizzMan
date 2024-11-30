6bf0f74c18e85a1f22c832e9c34b68121c44bee8
Arbath Abdurrahman <arbath21@gmail.com>
2024-11-30 18:12:19 +0700

    Memperbaiki forms.py

--- rizzman/rizzman/forms.py
+++ rizzman/rizzman/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import
+from .models import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import UserCreationForm
